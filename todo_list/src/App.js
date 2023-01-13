11a3d8252aecd8ea549b54996366745b762b3daa
Rip Nephils <RipNephils@gmail.com>
2023-01-13 18:29:41 -0300

    first commit

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,16 +1,41 @@
 import React from "react";
 import "./App.css";
-const App = () => {
-  const [todos, setTodos] = React.useState([]);
+
+export default function App() {
+  const [todoList, setTodoList] = React.useState([]);
   const [todo, setTodo] = React.useState("");
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
-};
-export default App;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let newTodo = {
+      key: new Date().getTime,
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+
+    if (newTodo.text.length > 0) {
+      setTodoList([...todoList].concat(newTodo));
+      setTodo("");
+      event.target.vale = "";
+    } else {
+      alert("Invalid task. Add a task with at least 1 character");
+      setTodo("");
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          align="right"
+          onChange={(event) => setTodo(event.target.value)}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+}
